4a5b5f719ea6151100b82152531172ef4744bdde
Dominik Czorniej <gunieq@gmail.com>
2021-06-10 11:17:52 +0200

    Finished work

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+valid_dices = {
+    "D3",
+    "D4",
+    "D6",
+    "D8",
+    "D10",
+    "D12",
+    "D20",
+    "D100",
+}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def cast_dice():
+    dice_formula = str(input("Enter the formula: "))
+    for dice in valid_dices:
+        if dice in dice_formula:
+            try:
+                multiply, modifier = dice_formula.split(dice)
+            except ValueError:
+                return "Wrong input"
+            dice_value = int(dice[1:])
+            break
+    else:
+        return "Wrong input"
+    try:
+        multiply = int(multiply) if multiply else 1
+    except ValueError:
+        return "Wrong Input"
+    try:
+        modifier = int(modifier) if modifier else 0
+    except ValueError:
+        return "Wrong Input"
+    return sum([random.randint(1, dice_value) for i in range(multiply)]) + modifier
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(cast_dice())
